55a2e18030253ee02b071614fd96853fc46ecbfb
Brennan Duck <brennantduck@gmail.com>
2024-01-25 03:49:17 -0500

    removed broken part in button component

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,13 +1,9 @@
 interface ButtonProps {
-    name: string;
-    val: number;
-    handlerFunc: () => void;
+  name: string;
+  val: number;
+  handlerFunc: () => void;
 }
-function Button({ name, val, handlerFunc }: ButtonProps) {
-    return (
-        <button onClick={handlerFunc}>
-            {name} is {val}
-        </button>
-    );
+function Button({ name, handlerFunc }: ButtonProps) {
+  return <button onClick={handlerFunc}>{name}</button>;
 }
 export default Button;
