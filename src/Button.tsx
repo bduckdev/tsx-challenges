ca49a7c3422599c7c5f91533973602ae279263fd
Brennan Duck <brennantduck@gmail.com>
2024-01-29 22:57:44 -0500

    fixed random type error

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,12 +2,12 @@ import { Count } from "./App";
 interface ButtonProps {
   name: string;
   val: number;
-  handlerFunc: (i: keyof Count) => void;
+  handlerFunc: (i: keyof Count[]) => void;
   i: keyof Count[];
 }
 function Button({ name, val, handlerFunc, i }: ButtonProps) {
   return (
-    <button onClick={() => handlerFunc(i as keyof Count)}>
+    <button onClick={() => handlerFunc(i as keyof Count[])}>
       {name} is {val}
     </button>
   );
