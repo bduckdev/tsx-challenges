957495696b6e920aa2849797236016acab4e8989
Brennan Duck <brennantduck@gmail.com>
2024-01-29 22:58:52 -0500

    added spice

--- src/App.tsx
+++ src/App.tsx
@@ -3,67 +3,68 @@ import "./App.css";
 import { useState } from "react";
 
 export interface Count {
-  name: string;
-  count: number;
+    name: string;
+    count: number;
 }
 
 const initialCounts: Count[] = [
-  {
-    name: "foo",
-    count: 68,
-  },
-  {
-    name: "bar",
-    count: 419,
-  },
-  {
-    name: "buzz",
-    count: 359,
-  },
-  {
-    name: "bazz",
-    count: 123,
-  },
+    {
+        name: "foo",
+        count: 68,
+    },
+    {
+        name: "bar",
+        count: 419,
+    },
+    {
+        name: "buzz",
+        count: 359,
+    },
+    {
+        name: "bazz",
+        count: 123,
+    },
 ];
 function App() {
-  const [counters, setCounters] = useState(initialCounts);
+    const [counters, setCounters] = useState(initialCounts);
 
-  function incrementCount(i: keyof Count[]) {
-    const newCounters: Count[] = [...initialCounts];
-    newCounters.map((item, index: keyof Count[]) => {
-      if (index === i) {
-        item.count = item.count + 1;
-      }
-      return item;
-    });
-    setCounters(newCounters);
-  }
+    function incrementCount(i: keyof Count[]) {
+        const newCounters: Count[] = [...initialCounts];
+        newCounters.map((item, index: keyof Count[]) => {
+            if (index === i) {
+                item.count = item.count + 1;
+            }
+            return item;
+        });
+        setCounters(newCounters);
+    }
 
-  function calcTotal() {
-    let total = 0;
-    initialCounts.forEach((item) => {
-      total = total + item.count;
-    });
-    return total;
-  }
-  return (
-    <>
-      <div>
-        <span>total count is {calcTotal()}</span>
-        {counters.map((item, i: keyof Count[]) => {
-          return (
-            <Button
-              key={item.name}
-              name={item.name}
-              val={item.count}
-              handlerFunc={incrementCount}
-              i={i}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
+    function calcTotal() {
+        let total = 0;
+        initialCounts.forEach((item) => {
+            total = total + item.count;
+        });
+        return total;
+    }
+    return (
+        <>
+            <h1>TSX CHALLENGES</h1>
+            <div>
+                <span>total count is {calcTotal()}</span>
+                {counters.map((item, i: keyof Count[]) => {
+                    return (
+                        <Button
+                            key={item.name}
+                            name={item.name}
+                            val={item.count}
+                            handlerFunc={incrementCount}
+                            i={i}
+                        />
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 
 export default App;
