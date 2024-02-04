d252383b48975f4f77d61e0a2df08b4cd879a476
Brennan Duck <brennantduck@gmail.com>
2024-02-04 01:27:42 -0500

    added context for counters instead of passing handlerFunc as a prop

--- src/routes/counters/App.tsx
+++ src/routes/counters/App.tsx
@@ -1,69 +1,69 @@
 import Button from "./Button";
 import "./App.css";
 import { useState } from "react";
+import CounterContext from "./CounterContext";
 
 export interface Count {
-    name: string;
-    count: number;
+  name: string;
+  count: number;
 }
 
 const initialCounts: Count[] = [
-    {
-        name: "foo",
-        count: 68,
-    },
-    {
-        name: "bar",
-        count: 419,
-    },
-    {
-        name: "buzz",
-        count: 359,
-    },
-    {
-        name: "bazz",
-        count: 123,
-    },
+  {
+    name: "foo",
+    count: 68,
+  },
+  {
+    name: "bar",
+    count: 419,
+  },
+  {
+    name: "buzz",
+    count: 359,
+  },
+  {
+    name: "bazz",
+    count: 123,
+  },
 ];
 function App() {
-    const [counters, setCounters] = useState(initialCounts);
+  const [counters, setCounters] = useState(initialCounts);
 
-    function incrementCount(i: keyof Count[]) {
-        const newCounters: Count[] = [...initialCounts];
-        newCounters.map((item, index: keyof Count[]) => {
-            if (index === i) {
-                item.count = item.count + 1;
-            }
-            return item;
-        });
-        setCounters(newCounters);
-    }
+  function incrementCount(i: keyof Count[]) {
+    const newCounters: Count[] = [...initialCounts];
+    newCounters.map((item, index: keyof Count[]) => {
+      if (index === i) {
+        item.count = item.count + 1;
+      }
+      return item;
+    });
+    setCounters(newCounters);
+  }
 
-    function calcTotal() {
-        let total = 0;
-        initialCounts.forEach((item) => {
-            total = total + item.count;
-        });
-        return total;
-    }
-    return (
-        <>
-            <div>
-                <span>total count is {calcTotal()}</span>
-                {counters.map((item, i: keyof Count[]) => {
-                    return (
-                        <Button
-                            key={item.name}
-                            name={item.name}
-                            val={item.count}
-                            handlerFunc={incrementCount}
-                            i={i}
-                        />
-                    );
-                })}
-            </div>
-        </>
-    );
+  function calcTotal() {
+    let total = 0;
+    initialCounts.forEach((item) => {
+      total = total + item.count;
+    });
+    return total;
+  }
+  return (
+    <>
+      <div>
+        <span>total count is {calcTotal()}</span>
+        {counters.map((item, i: keyof Count[]) => {
+          return (
+            <CounterContext.Provider
+              key={item.name}
+              value={{ handlerFunc: incrementCount }}
+            >
+              <Button name={item.name} val={item.count} i={i} />
+            </CounterContext.Provider>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/routes/counters/Button.tsx
+++ src/routes/counters/Button.tsx
@@ -1,13 +1,16 @@
 import { Count } from "./App";
+import CounterContext from "./CounterContext";
+import { useContext } from "react";
+
 interface ButtonProps {
   name: string;
   val: number;
-  handlerFunc: (i: keyof Count[]) => void;
   i: keyof Count[];
 }
-function Button({ name, val, handlerFunc, i }: ButtonProps) {
+function Button({ name, val, i }: ButtonProps) {
+  const handlerFunc = useContext(CounterContext).handlerFunc;
   return (
-    <button onClick={() => handlerFunc(i as keyof Count[])}>
+    <button onClick={() => handlerFunc(i)}>
       {name} is {val}
     </button>
   );
