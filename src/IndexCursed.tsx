38c486d614ca2669e7d3f9e0814a90115e26a16a
Brennan Duck <brennantduck@gmail.com>
2024-02-07 12:06:39 -0500

    added many things

---
+++ src/IndexCursed.tsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { beeMovieScriptArr } from "./beeMovieScript";
+import { beeMovieScriptByWordArr } from "./beeMovieScriptByWord";
+import { beeMovieScriptByCharArr } from "./beeMovieScriptByChar";
+
+function Index() {
+  const [beeMovieScript, setBeeMovieScript] = useState<boolean>(false);
+  const [beeMovieScriptByWord, setBeeMovieScriptByWord] =
+    useState<boolean>(false);
+  const [beeMovieScriptByChar, setBeeMovieScriptByChar] =
+    useState<boolean>(false);
+
+  return (
+    <div className="py-3">
+      {/*
+CURSED
+CURSED
+CURSED
+CURSED
+CURSED
+CURSED
+CURSED
+          */}
+      <h1 className="text-4xl font-bold text-center mt-10">TSX Challenges</h1>
+      {beeMovieScript ? (
+        <button
+          onClick={() => setBeeMovieScript(false)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Hide Entire Bee Movie Script
+        </button>
+      ) : (
+        <button
+          onClick={() => setBeeMovieScript(true)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Show Entire Bee Movie Script
+        </button>
+      )}
+      {beeMovieScript && (
+        <ul>
+          {beeMovieScriptArr.map((line: string, index: number) => (
+            <li key={index}>{line}</li>
+          ))}
+        </ul>
+      )}
+      {beeMovieScriptByWord ? (
+        <button
+          onClick={() => setBeeMovieScriptByWord(false)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Hide Entire Bee Movie Script by Word
+        </button>
+      ) : (
+        <button
+          onClick={() => setBeeMovieScriptByWord(true)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Show Entire Bee Movie Script by Word
+        </button>
+      )}
+      {beeMovieScriptByWord && (
+        <ul>
+          {beeMovieScriptByWordArr.map((line: string, index: number) => (
+            <li key={index}>{line}</li>
+          ))}
+        </ul>
+      )}
+      {beeMovieScriptByChar ? (
+        <button
+          onClick={() => setBeeMovieScriptByChar(false)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Hide Entire Bee Movie Script by Char
+        </button>
+      ) : (
+        <button
+          onClick={() => setBeeMovieScriptByChar(true)}
+          className="bg-black mx-3 rounded-xl text-2xl underline underline-offset-4 p-4 font-medium text-gray-100"
+        >
+          Show Entire Bee Movie Script by Char
+        </button>
+      )}
+      {beeMovieScriptByChar && (
+        <ul>
+          {beeMovieScriptByCharArr.map((line: string, index: number) => (
+            <li key={index}>{line}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Index;
--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,22 @@ import ReactDOM from "react-dom/client";
 import Counters from "./routes/counters/App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavBar from "./NavBar";
 import Index from "./Index";
 import Cards from "./routes/cards/App";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Index /> },
-  { path: "/counters", element: <Counters /> },
-  { path: "/cards", element: <Cards /> },
+  {
+    path: "/",
+    element: <Index />,
+    children: [
+      { path: "/counters", element: <Counters /> },
+      { path: "/cards", element: <Cards /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar />
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
